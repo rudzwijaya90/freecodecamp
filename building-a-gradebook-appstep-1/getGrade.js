c04d978f2799f17555e611e90424c8411baa617a
Rudi <rudzwijaya90@gmail.com>
2024-09-16 23:08:35 +0700

    cara awal ditaruh di bawah

--- building-a-gradebook-appstep-1/getGrade.js
+++ building-a-gradebook-appstep-1/getGrade.js
@@ -42,3 +42,29 @@ console.log(names[2] + ": " + getGrade(96));
 console.log(getGrade(82));
 console.log(getGrade(56));
 console.log(getGrade(100));
+
+//Cara Awal dengan if..else if..else
+function getGrade(score) {
+  if(score < 0 || score > 100) return "Invalid Score"; //check if score within 0 - 100
+
+  if(score === 100){
+    return "A++";
+  } else if (score >= 90){
+    return "A";
+  } else if (score >= 80){
+    return "B";
+  } else if (score >= 70){
+    return "C";
+  } else if (score >= 60){
+    return "D";
+  } else{
+    return "F";
+  }
+}
+
+const name = ["Budi", "Andi", "Aray", "Ikal"];
+
+console.log(name[2] + ": " + getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
+console.log(getGrade(100));
