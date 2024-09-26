fb4c5dab69502f1e38acdee374a86526aba13b41
Rudi <rudzwijaya90@gmail.com>
2024-09-26 23:35:23 +0700

    add seconds just for fun

--- Calendar or Date Formatter/script.js
+++ Calendar or Date Formatter/script.js
@@ -7,3 +7,33 @@ const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const formattedDate = `${day}-${month}-${year}`;
+//console.log(formattedDate);
+
+currentDateParagraph.textContent = formattedDate;
+
+
+
+//change event is used to detect when the value of an HTML element has changed
+dateOptionsSelectElement.addEventListener('change', () => { 
+    switch (dateOptionsSelectElement.value) {
+        case "yyyy-mm-dd":
+          currentDateParagraph.textContent = formattedDate
+            .split("-")
+            .reverse()
+            .join("-");
+          break;
+        case "mm-dd-yyyy-h-mm":
+          currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+          break;
+        case "mm-dd-yyyy-h-mm-ss":
+            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+            break;
+        default:
+            currentDateParagraph.textContent = formattedDate;
+    }
+});
+
+// const exampleSentence = "selur pmaCedoCeerf".split('').reverse().join('');
+// console.log(exampleSentence);
