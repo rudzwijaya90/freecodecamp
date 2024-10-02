ff4b370690182ac8afa3c04fed4e5b33106837bc
Rudi <rudzwijaya90@gmail.com>
2024-10-02 17:08:47 +0700

    step 18

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -2,13 +2,18 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
 
+};
 
 function checkUserInput = () => {
-    if (!numberInput.value){
-
-    }
-    console.log(numberInput.value);
+    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0){
+        alert('Please provide a decimal number greater than or equal to 0');
+        return;
+    }  
+    //console.log(numberInput.value);
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
 };
 
 convertBtn.addEventListener('click', checkUserInput);
