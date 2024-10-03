4f578ebfb57346227dfe9a6e96dac403dbf15eb1
Rudi <rudzwijaya90@gmail.com>
2024-10-03 19:04:50 +0700

    step 41

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -3,7 +3,33 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
+    //return "1"; //step 18 return string of 1 or true, false is 0 in binary
+    /* const inputs = [];
+    const quotients = [];
+    const remainders = [];
+    if(input === 0){
+        result.innerText = "0";
+        return;
+    };
+    while (input > 0){
+        const quotient = Math.floor(input / 2);
+        const remainder = input % 2;
 
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
+        input = quotient;
+    }
+    console.log("Inputs: ", inputs);
+    console.log("Quotients: ", quotients);
+    console.log("Remainders: ", remainders);
+    result.innerText = remainders.reverse().join(""); */
+    let binary = "";
+    while(input > 0){
+        binary = input % 2 + binary;
+        input = Math.floor(input / 2);
+      }  
+    result.innerText = binary;
 };
 
 function checkUserInput = () => {
