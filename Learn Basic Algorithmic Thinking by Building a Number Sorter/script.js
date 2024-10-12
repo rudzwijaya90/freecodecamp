0d0fef0d18f54607047f1383a65d0e357f15676f
Rudi <rudzwijaya90@gmail.com>
2024-10-12 10:43:49 +0700

    Number Sorter first commit

---
+++ Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
@@ -0,0 +1,6 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault(); //to prevent default click action button inside form element
+}
+
