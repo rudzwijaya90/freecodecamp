a36ef9a322ac04f054270b7e3b170efe66df1a2e
Rudi <rudzwijaya90@gmail.com>
2024-10-23 02:26:00 +0700

    build a spreadsheet

---
+++ Building a Spreadsheet/script.js
@@ -0,0 +1,56 @@
+/* const isEven = (num) => {
+    if (num % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+} */
+const isEven = num => num % 2 === 0;
+
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+const median = nums => {
+    const sorted = nums.slice().sort((a,b) => a - b); //sorting in ascending order
+    const length = sorted.length;
+    const middle = length / 2 - 1;
+    return isEven(length) ? 
+        average([sorted[middle], sorted[middle + 1]]) : 
+        sorted[Math.ceil(middle)];  
+}
+ 
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+    
+const charRange = (start, end) => 
+    range(start.charCodeAt(0), end.charCodeAt(0)) //step 12
+    .map(code => String.fromCharCode(code));
+
+
+
+
+
+
+
+
+
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  };
+  const letters = charRange("A", "J")
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.id = letter + number;
+        input.ariaLabel = letter + number;
+        container.appendChild(input);
+    });
+  });
+};
