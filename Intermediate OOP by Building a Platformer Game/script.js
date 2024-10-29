7b32b2f74186d2ed69e23f232bd52d5c3de71e4e
Rudi <rudzwijaya90@gmail.com>
2024-10-29 23:32:27 +0700

    step 111

--- Intermediate OOP by Building a Platformer Game/script.js
+++ Intermediate OOP by Building a Platformer Game/script.js
@@ -56,11 +56,80 @@ class Player {
     }
   }
 
+  //Platform Logic
+  class Platform {
+    constructor (x, y) {
+      this.position = {
+        x, y
+      }
+      this.width = 200;
+      this.height = proportionalSize(40) //proportionalSize() function to make sure the height is proportional to the screen size
+    }
+      draw(){
+        ctx.fillStyle = "#acd157";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+      }
+  }
+
+  //CheckPoint Logic
+  class CheckPoint {
+    constructor (x, y, z) {
+      this.position = {
+        x,
+        y,
+      };
+      this.width = proportionalSize(40);
+      this.height = proportionalSize(70);
+      this.claimed = false;
+    };
+    draw(){
+      ctx.fillStyle = "#f1be32";
+      ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    };
+    claim(){
+      this.width = 0;
+      this.height = 0;
+      this.position.y = Infinity;
+      this.claimed = true;
+    };
+  };
+
   const player = new Player(); //new Player() object
+  const platformPositions = [
+    { x: 500, y: proportionalSize(450) },
+    { x: 700, y: proportionalSize(400) },
+    { x: 850, y: proportionalSize(350) },
+    { x: 900, y: proportionalSize(350) },
+    { x: 1050, y: proportionalSize(150) },
+    { x: 2500, y: proportionalSize(450) },
+    { x: 2900, y: proportionalSize(400) },
+    { x: 3150, y: proportionalSize(350) },
+    { x: 3900, y: proportionalSize(450) },
+    { x: 4200, y: proportionalSize(400) },
+    { x: 4400, y: proportionalSize(200) },
+    { x: 4700, y: proportionalSize(150) }
+  ];
+
+  const platforms = platformPositions.map(
+    platform => new Platform(platform.x, platform.y) 
+  );
+
+  const checkpointPositions = [
+    { x: 1170, y: proportionalSize(80), z: 1 },
+    { x: 2900, y: proportionalSize(330), z: 2 },
+    { x: 4800, y: proportionalSize(80), z: 3 } 
+  ];
+
+  const checkpoints = checkpointPositions.map(
+    checkpoint => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z)
+  );
 
   const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clearRect() Web API
+    platforms.forEach((platform) => {platform.draw()});
+    checkpoints.forEach((checkpoint) => {checkpoint.draw()});
+    
     player.update();
     if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
       player.velocity.x = 5;
@@ -68,12 +137,57 @@ class Player {
       player.velocity.x = -5;
     } else {
       player.velocity.x = 0;
+      if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+        platforms.forEach((platform) => {
+          platform.position.x -= 5
+        });
+        checkpoints.forEach((checkpoint) => { //step 103
+          checkpoint.position.x -= 5;
+        });
+      } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive ) {
+          platforms.forEach((platform) => {
+            platform.position.x += 5;
+          });
+          checkpoints.forEach((checkpoint) => {
+            checkpoint.position.x += 5;
+          });
+      }
     }
+    platforms.forEach((platform) => {
+      const collisionDetectionRules = [
+        player.position.y + player.height <= platform.position.y,
+        player.position.y + player.height + player.velocity.y >= platform.position.y,
+        player.position.x >= platform.position.x - player.width / 2,
+        player.position.x <= platform.position.x + platform.width - player.width / 3
+      ];
+      if (collisionDetectionRules.every((rule) => rule)) {
+        player.velocity.y = 0;
+        return;
+      }
+      const platformDetectionRules = [
+        player.position.x >= platform.position.x - player.width / 2,
+        player.position.x <= platform.position.x + platform.width - player.width / 3,
+        player.position.y + player.height >= platform.position.y,
+        player.position.y <= platform.position.y + platform.height,
+      ];
+      if (platformDetectionRules.every((rule) => rule)) {
+        player.position.y = platform.position.y + player.height;
+        player.velocity.y = gravity;
+      };
+    });
+    checkpoints.forEach((checkpoint, index, checkpoints) => {
+      const checkpointDetectionRules = [
+        player.position.x >= checkpoint.position.x,
+        player.position.y >= checkpoint.position.y,
+        player.position.y + player.height <= checkpoint.position.y + checkpoint.height,
+        isCheckpointCollisionDetectionActive;
+      ];
+    });
   };
 
   const keys = {
     rightKey: {pressed: false},
-    leftKey: {pressed: false},
+    leftKey: {pressed: false}
   };
 
   const movePlayer = (key, xVelocity, isPressed) => {
@@ -109,6 +223,14 @@ class Player {
     startScreen.style.display = "none";
     //player.draw();
     animate();
+  };
+
+  const showCheckpointScreen = (msg) => {
+    checkpointScreen.style.display = "block";
+    checkpointMessage.textContent = msg;
+    if (isCheckpointCollisionDetectionActive) {
+      setTimeout(() => (checkpointScreen.style.display = "none"), 2000);
+    };
   };
 
   startBtn.addEventListener("click", startGame);
